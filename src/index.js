76996e065f6d9417b56a2600e980ffdbaedfb2fd
cunhang_wwei <cunhang_wwei@yunzhijia.com>
2021-07-03 17:47:59 +0800

    feat: update example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-06-30 16:17:10
  * @LastEditors: cunhang_wwei
- * @LastEditTime: 2021-07-03 17:09:36
+ * @LastEditTime: 2021-07-03 17:47:23
  * @Description: 主入口
  */
 
@@ -17,6 +17,6 @@ const templateString = `
     </ul>
 </div>
 `
-
+console.log('输入的模板字符串', templateString);
 const ast = parse(templateString)
-console.log('AST', ast)
+console.log('生成的AST\n', ast)
